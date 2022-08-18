33ac74669e9377de7df348fd3c487e59d0c3b578
Aline Grance <grance.aline@gmail.com>
2022-08-18 17:37:58 -0300

    Done add basic style

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,8 +6,8 @@ class PlanetCard extends React.Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card" className="flex-planet">
-        <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <h3 data-testid="planet-name">{planetName}</h3>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } className="planet-img" />
       </div>
     );
   }
